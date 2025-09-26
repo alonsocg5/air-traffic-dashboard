1ac44cedd4fa334fdf342bfd9af769c39c2337b0
alonsocg5 <alonso.castagon@gmail.com>
2025-09-26 11:59:12 +0200

    Obtención de los datos y limpueza del dataset completa

--- get_data.py
+++ get_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import utils
 
+# --------------------- Obtenención de los datos de OpenSky ---------------------
 
 # Columnas del DataFrame
 columns = [
@@ -30,3 +31,37 @@ df = pd.DataFrame(data.get("states", []), columns=columns)
 df.to_csv("data/flights_raw.csv", index=False)
 print("Datos guardados en data/flights_raw.csv")
 print(df.head())
+
+
+# --------------------- Limpieza del dataset --------------------- 
+
+clean_data = pd.read_csv("data/flights_raw.csv", sep=",", decimal='.')
+
+# Quitar espacios en blanco en callsign
+clean_data["callsign"] = clean_data["callsign"].str.strip()
+
+# Eliminar filas con latitud y longitud nulas (no se conoce la posición)
+clean_data = clean_data.dropna(subset=["latitude", "longitude"])
+
+# Eliminar columna 'sensors' porque está vacía
+clean_data = clean_data.drop(columns=["sensors"])
+
+# Rellenar callsign nulos con UNKNOWN ya que es el identificador de vuelo
+clean_data["callsign"] = clean_data["callsign"].fillna("UNKNOWN").str.strip()
+
+# Eliminación de duplicados
+clean_data = clean_data.drop_duplicates()
+
+# Número de valores nulos en cada variable
+print(clean_data.shape)
+n_nan = clean_data.isna().sum()
+print("- Número de valores nulos en cada variable:")
+print(n_nan, "\n")
+
+# Guardar limpio
+df.to_csv("data/flights_clean.csv", index=False)
+
+print("Datos limpios guardados en data/flights_clean.csv")
+
+
+
